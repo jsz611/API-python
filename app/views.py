b4a1d12a65d157d639f31732fa4a38292ebc5465
Josiel Souza <souzajosiel611@gmail.com>
2023-06-18 21:48:30 -0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+class HomeApiView(APIView):
+    def get(self, request, format=None):
+        return Response({"nome": "Josiel Souza", "Idade": 35}, status=200)
